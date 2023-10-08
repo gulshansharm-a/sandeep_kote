935940d665d1dcbefd3ad495d37b02ed4da8b3b9
Rhitam Nayek <rhitamnayek17@gmail.com>
2023-10-08 19:59:27 +0530

    table

--- src/Components/DashBoard/DashBoardComponents/TransactionHIstory.jsx
+++ src/Components/DashBoard/DashBoardComponents/TransactionHIstory.jsx
@@ -1,20 +1,53 @@
-// TransactionHistory.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ref, onValue, push } from 'firebase/database';
+import { database } from '../../../Authentication/firebase'; // Update this path
 
-const TransactionHistory = ({ transactions }) => {
+const TransactionHistory = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const transactionsRef = ref(database, 'TransactionHistory');
+
+    const unsubscribe = onValue(transactionsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const transactionsArray = Object.values(data);
+        setTransactions(transactionsArray);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div className="lg:ml-40 mt-5 font-serif text-2xl">
       <h3>Transaction History:</h3>
       <ul>
+        <thead>
+        <tr>
+                        <th className="p-3 border">Amount</th>
+                        <th className="p-3 border">Type</th>
+                        <th className="p-3 border">Recipient Role</th>
+                        <th className="p-3 border">Recipient Email</th>
+                        <th className="p-3 border">Timestamp</th>
+
+                    </tr>
+        </thead>
+        <tbody>
         {transactions.map((transaction, index) => (
-          <li key={index}>
-            <strong>Amount:</strong> {transaction.amount},{' '}
-            <strong>Type:</strong> {transaction.transactionType},{' '}
-            <strong>Recipient Role:</strong> {transaction.recipientRole},{' '}
-            <strong>Recipient Email:</strong> {transaction.recipientEmail},{' '}
-            <strong>Timestamp:</strong> {transaction.timestamp}
-          </li>
+          <tr key={index}>
+            
+            <td className="p-3 border">{transaction.amount},{' '}</td> 
+            <td className="p-3 border">{transaction.transactionType},{' '}</td> 
+            <td className="p-3 border">{transaction.recipientRole},{' '}</td> 
+            <td className="p-3 border">{transaction.recipientEmail},{' '}</td> 
+            <td className="p-3 border">{transaction.timestamp}</td> 
+
+          </tr>
         ))}
+        </tbody>
       </ul>
     </div>
   );

21f42ffad98cc40b5c0540f51a8c3f6fec9b822a
nivivirat <imniveditha1188@gmail.com>
2023-10-10 21:46:36 +0530

    made changes in loading screen

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,13 +1,16 @@
+// Loading.js
 import React from 'react';
-import 'tailwindcss/tailwind.css'; // Import Tailwind CSS styles
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      {/* Loading Spinner */}
-      <div className="inline-block animate-spin rounded-full border-t-4 border-gray-900 border-t-gray-100 h-16 w-16"></div>
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <div className="text-white text-4xl font-semibold">
+        Loading...
+      </div>
+      <div className="ml-4 border-t-4 border-white border-solid rounded-full animate-spin w-8 h-8"></div>
     </div>
   );
 };
+
 
 export default LoadingSpinner;

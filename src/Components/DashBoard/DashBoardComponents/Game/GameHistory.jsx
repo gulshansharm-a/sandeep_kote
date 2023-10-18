5c8a12b0552aaf7f577d7631ea96aed4f95eb585
nivivirat <imniveditha1188@gmail.com>
2023-10-18 07:40:35 +0530

    gameHistory done

--- src/Components/DashBoard/DashBoardComponents/Game/GameHistory.jsx
+++ src/Components/DashBoard/DashBoardComponents/Game/GameHistory.jsx
@@ -1,121 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { auth, database } from '../../../../Authentication/firebase';
-import { get, ref } from 'firebase/database';
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
-export default function GameHistory() {
-  const [userRole, setUserRole] = useState(null);
-  const [userEmail, setUserEmail] = useState(null);
-  const [currentUserID, setCurrentUserID] = useState(null);
-  const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState('');
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-lRLBHee1IISE8t5pJywkP-YrHPKIvk4',
+  authDomain: 'sandeepkote-c67f5.firebaseapp.com',
+  databaseURL: 'https://sandeepkote-c67f5-default-rtdb.firebaseio.com',
+  projectId: 'sandeepkote-c67f5',
+  storageBucket: 'sandeepkote-c67f5.appspot.com',
+  messagingSenderId: '871561614523',
+  appId: '1:871561614523:web:3b12ae93e7490723ddc59e',
+  measurementId: 'G-645LW1SWKT',
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const GameHistory = () => {
+  const [playerData, setPlayerData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
-    const fetchData = async () => {
-        try {
-          const snapshot = await get(ref(database));
-          const data = snapshot.val();
-      
-          console.log('Fetched data from Firebase:', data);
-      
-          let foundUserRole = null;
-          let foundUserEmail = null;
-      
-          for (const role in data) {
-            if (data[role][auth.currentUser.uid]) {
-              foundUserRole = role;
-              foundUserEmail = auth.currentUser.email;
-              setCurrentUserID(auth.currentUser.uid);
-              break;
-            }
-          }
-      
-          console.log('Found User Role:', foundUserRole);
-          console.log('Found User Email:', foundUserEmail);
-      
-          setUserRole(foundUserRole);
-          setUserEmail(foundUserEmail);
-      
-          if (foundUserRole) {
-            const userRoleData = data[foundUserRole];
-      
-            console.log('User Role Data:', userRoleData);
-      
-            // Check if the user has data for their role
-            if (userRoleData && auth.currentUser.uid in userRoleData) {
-              const userDataForUser = userRoleData[auth.currentUser.uid];
-      
-              console.log('User Data for User:', userDataForUser);
-      
-              // Check if the user has the required ID
-              if (userDataForUser && 'adminID' in userDataForUser) {
-                const adminOrDistributorID = userDataForUser.adminID;
-      
-                console.log('Admin or Distributor ID:', adminOrDistributorID);
-      
-                // Check if the admin or distributor ID is present
-                if (adminOrDistributorID) {
-                  const playerSnapshot = await get(ref(database, `Player/${adminOrDistributorID}`));
-      
-                  const playerData = playerSnapshot.val();
-      
-                  console.log('Player Data:', playerData);
-      
-                  // Assuming the player data has a 'bet' field
-                  setUserData(playerData);
-                } else {
-                  console.error('Admin or Distributor ID not found for the user.');
-                }
-              } else {
-                console.error('User data does not contain key: adminID');
-              }
-            } else {
-              console.error(`Role data not found for role: ${foundUserRole}`);
-            }
-      
-            setLoading(false);
-          } else {
-            console.error('User role not found.');
-          }
-      
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching data from Firebase:', error.message);
-          setLoading(false);
-        }
-      };
-      
-    fetchData();
-  }, [searchTerm]);
+    const playerRef = ref(database, 'Player');
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    const unsubscribe = onValue(playerRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const dataArray = Object.keys(data).map((uid) => {
+          const player = data[uid];
+          const betKey = Object.keys(player?.bet || {})[0];
+          const betData = player?.bet?.[betKey] || {};
 
-  // Assuming you have betIds and betDetails fetched from Firebase
-  const betIds = userData?.bet ? Object.keys(userData.bet) : [];
-  const betDetails = userData?.bet || [];
+          return {
+            email: player.email || '-',
+            chapa: betData?.chapa || '-',
+            kata: betData?.kata || '-',
+            result: betData?.result || '-',
+            time: betData?.time || '-',
+          };
+        });
 
-  // Pagination logic
-  const indexOfLastItem = currentPage * rowsPerPage;
-  const indexOfFirstItem = indexOfLastItem - rowsPerPage;
+        setPlayerData(dataArray);
+      }
+    });
 
-  const filteredBetIds = betIds.filter((betId) => {
-    const bet = betDetails[betId];
-    const searchTermLower = searchTerm.toLowerCase();
-
-    return (
-      betId.includes(searchTermLower) ||
-      bet.chapa.toString().includes(searchTermLower) ||
-      bet.kata.toString().includes(searchTermLower) ||
-      bet.result.includes(searchTermLower) ||
-      bet.time.includes(searchTermLower)
-    );
-  });
-
-  const currentBetIds = filteredBetIds.slice(indexOfFirstItem, indexOfLastItem);
+    return () => {
+      unsubscribe();
+    };
+  }, [database]);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
@@ -126,59 +59,34 @@ export default function GameHistory() {
     setCurrentPage(1);
   };
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-    setCurrentPage(1); // Reset to the first page when changing the search term
-  };
+  const indexOfLastItem = currentPage * rowsPerPage;
+  const indexOfFirstItem = indexOfLastItem - rowsPerPage;
+
+  const currentPlayers = playerData.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
-    <div>
-      <h2>Game History for {userData?.userName}</h2>
-
-      <div className="mb-4">
-        <label className="block text-gray-900 font-bold text-lg mb-2" htmlFor="search">
-          Search:
-        </label>
-        <input
-          type="text"
-          id="search"
-          value={searchTerm}
-          onChange={handleSearch}
-          className="mt-1 p-2 border rounded w-full focus:outline-none focus:ring focus:border-blue-500"
-          placeholder="Search by Bet ID, Chapa, Kata, Result, or Time"
-        />
-      </div>
-
-      <table className="table-auto">
+    <div className="lg:ml-20 mt-5 font-serif">
+      <h2 className='font-bold'>Game History</h2>
+      <table className="mt-4 w-full border">
         <thead>
           <tr>
-            <th className="border px-4 py-2">Player Email</th>
-            <th className="border px-4 py-2">Player Username</th>
-            <th className="border px-4 py-2">Balance</th>
-            <th className="border px-4 py-2">Bet ID</th>
-            <th className="border px-4 py-2">Chapa</th>
-            <th className="border px-4 py-2">Kata</th>
-            <th className="border px-4 py-2">Result</th>
-            <th className="border px-4 py-2">Time</th>
+            <th className="p-3 border">Email</th>
+            <th className="p-3 border">Chapa</th>
+            <th className="p-3 border">Kata</th>
+            <th className="p-3 border">Result</th>
+            <th className="p-3 border">Time</th>
           </tr>
         </thead>
         <tbody>
-          {currentBetIds.map((betId) => {
-            const bet = betDetails[betId];
-
-            return (
-              <tr key={betId}>
-                <td className="border px-4 py-2">{userEmail}</td>
-                <td className="border px-4 py-2">{userData?.userName}</td>
-                <td className="border px-4 py-2">{userData?.balance}</td>
-                <td className="border px-4 py-2">{betId}</td>
-                <td className="border px-4 py-2">{bet.chapa}</td>
-                <td className="border px-4 py-2">{bet.kata}</td>
-                <td className="border px-4 py-2">{bet.result}</td>
-                <td className="border px-4 py-2">{bet.time}</td>
-              </tr>
-            );
-          })}
+          {currentPlayers.map((player, index) => (
+            <tr key={index}>
+              <td className="p-3 border">{player.email}</td>
+              <td className="p-3 border">{player.chapa}</td>
+              <td className="p-3 border">{player.kata}</td>
+              <td className="p-3 border">{player.result}</td>
+              <td className="p-3 border">{player.time}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
@@ -200,10 +108,9 @@ export default function GameHistory() {
         </select>
 
         <div className="mt-2 flex justify-center space-x-2">
-          {/* Centering the pagination buttons horizontally */}
-          {Array.from({ length: Math.ceil(filteredBetIds.length / rowsPerPage) }).map((_, index) => (
+          {Array.from({ length: Math.ceil(playerData.length / rowsPerPage) }).map((_, index) => (
             <button
-              key={index}
+              key={index + 1}
               onClick={() => handlePageChange(index + 1)}
               className={`${
                 currentPage === index + 1 ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'
@@ -216,4 +123,6 @@ export default function GameHistory() {
       </div>
     </div>
   );
-}
+};
+
+export default GameHistory;
